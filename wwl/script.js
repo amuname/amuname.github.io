adf030c75a404656c6062da0ecef63d119840d67
amuname <74045707+amuname@users.noreply.github.com>
2020-11-23 16:17:28 +0300

    Add files via upload

---
+++ wwl/script.js
@@ -0,0 +1,22 @@
+function toSrc3(){
+	var scr = document.querySelector('#scr3');
+	scr.scrollIntoView({behavior: "smooth"});
+}
+function toMenu(e) {
+	var m1 = document.querySelector('#m1');
+	var m2 = document.querySelector('#m2');
+	var m3 = document.querySelector('#m3');
+	if (e==1) {
+		m1.scrollIntoView({behavior: "smooth"});
+	}
+	if (e==2) {
+		m2.scrollIntoView({behavior: "smooth"});
+	}
+	if (e==3) {
+		m3.scrollIntoView({behavior: "smooth"});
+	}
+}
+function toTop() {
+	var top = document.querySelector('h1');
+	top.scrollIntoView({behavior: "smooth"});
+}
